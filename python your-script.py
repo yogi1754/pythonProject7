bd6511535c41b609e8a20f5f4563fe0ece6dc981
yogi1754 <donyogeshwar@gmail.com>
2023-04-11 22:02:31 +0100

    Create python your-script.py

---
+++ python your-script.py
@@ -0,0 +1,37 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage('Clone repository') {
+            steps {
+                git branch: 'main', url: 'https://github.com/yourusername/your-repository.git'
+            }
+        }
+        
+        stage('Install dependencies') {
+            steps {
+                sh 'pip install pymongo pandas numpy scikit-learn matplotlib seaborn'
+            }
+        }
+        
+        stage('Run script') {
+            steps {
+                sh 'python your-script.py'
+            }
+        }
+        
+        stage('Publish results') {
+            steps {
+                archiveArtifacts artifacts: 'amazon_reviews_us_Gift_Card_v1_00.tsv.gz'
+                publishHTML(target: [
+                    allowMissing: false,
+                    alwaysLinkToLastBuild: false,
+                    keepAll: true,
+                    reportDir: 'reports',
+                    reportFiles: 'index.html',
+                    reportName: 'Amazon Reviews'
+                ])
+            }
+        }
+    }
+}
