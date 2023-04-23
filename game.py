ed8d8a8dee3b9c0651706d668a0b3ef89a9546d6
yogi1754 <donyogeshwar@gmail.com>
2023-04-23 01:45:15 +0100

    Update game.py

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Connect to MongoDB
-client = pymongo.MongoClient("mongodb+srv://donyogeshwar:Welcome123@yogi.arb1cl7.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient(f"mongodb+srv://donyogeshwar:Welcome123@yogi.arb1cl7.mongodb.net/?retryWrites=true&w=majority")
 database_name = 'amazon_reviews'
 collection_name = 'gift_cards'
 db = client[database_name]
