b346104d812a57c6f0a058cc5f22c4b1264bca12
yogi1754 <donyogeshwar@gmail.com>
2023-04-27 23:28:08 +0100

    Update methmi.py

--- methmi.py
+++ methmi.py
@@ -105,35 +105,27 @@ plt.title('Distribution of Star Ratings')
 plt.xlabel('Star Rating')
 plt.ylabel('Count')
 
-# Display the histogram
-plt.show()
+plt.savefig(r'C:\UsC:\\Users\\donyo\\OneDrive\\Documents\\images\\methmi/histogram.png')
+plt.close()
 
 # plot scatter plot of star rating vs helpful votes
 plt.scatter(df['star_rating'], df['helpful_votes'])
 plt.title('Star Rating vs Helpful Votes')
 plt.xlabel('Star Rating')
 plt.ylabel('Helpful Votes')
-plt.show()
+
+plt.savefig(r'C:\UsC:\\Users\\donyo\\OneDrive\\Documents\\images\\methmi/scatter_plot1.png')
+plt.close()
 
 # plot scatter plot of star rating vs total votes
 plt.scatter(df['star_rating'], df['total_votes'])
 plt.title('Star Rating vs Total Votes')
 plt.xlabel('Star Rating')
 plt.ylabel('Total Votes')
-plt.show()
 
-# Convert review_date to datetime type
-df['review_date'] = pd.to_datetime(df['review_date'])
+plt.savefig(r'C:\UsC:\\Users\\donyo\\OneDrive\\Documents\\images\\methmi/scatter_plot.png')
 
-# Group by review_date and calculate the mean star_rating for each date
-date_grouped = df.groupby('review_date')['star_rating'].mean()
-
-# Plot the average star rating over time
-plt.plot(date_grouped.index, date_grouped.values)
-plt.title('Average Star Rating Over Time')
-plt.xlabel('Date')
-plt.ylabel('Average Star Rating')
-plt.show()
+plt.close()
 
 df = pd.read_sql_query("SELECT verified_purchase FROM amazon_sw", cnxn)
 
@@ -145,36 +137,10 @@ plt.bar(verified_counts.index, verified_counts.values)
 plt.title('Verified vs Non-Verified Purchases')
 plt.xlabel('Verified Purchase')
 plt.ylabel('Number of Purchases')
-plt.show()
 
-df = pd.read_sql_query("SELECT product_title, star_rating FROM amazon_sw", cnxn)
+plt.savefig(r'C:\UsC:\\Users\\donyo\\OneDrive\\Documents\\images\\methmi/Bar_Graph.png')
 
-# Group the data by product ID and calculate the mean star rating
-df_grouped = df.groupby('product_title')['star_rating'].mean().reset_index()
-
-# Sort the data by mean star rating in descending order
-df_grouped = df_grouped.sort_values('star_rating', ascending=False)
-
-# Plot the bar graph
-plt.figure(figsize=(10, 6))
-plt.bar(df_grouped['product_title'], df_grouped['star_rating'])
-plt.title('Average Star Rating by product title')
-plt.xlabel('Product Title')
-plt.ylabel('Average Star Rating')
-plt.xticks(rotation=90)
-plt.show()
-
-df = pd.read_sql_query("SELECT product_title, star_rating FROM amazon_sw", cnxn)
-# Create a box plot of star rating by product category
-df.boxplot(column='star_rating', by='product_title', figsize=(10, 6))
-
-# Set axis labels and title
-plt.xlabel('Product Category')
-plt.ylabel('Star Rating')
-plt.title('Distribution of Star Ratings by Product Title')
-
-# Show the plot
-plt.show()
+plt.close()
 
 df = pd.read_sql_query("SELECT review_headline FROM amazon_sw", cnxn)
 
@@ -189,7 +155,9 @@ plt.figure(figsize=(8, 8), facecolor=None)
 plt.imshow(wordcloud)
 plt.axis("off")
 plt.tight_layout(pad=0)
-plt.show()
+
+plt.savefig(r'C:\UsC:\\Users\\donyo\\OneDrive\\Documents\\images\\methmi/WordCloud1.png')
+plt.close()
 
 df = pd.read_sql_query("SELECT review_body FROM amazon_sw", cnxn)
 
@@ -204,9 +172,10 @@ plt.figure(figsize=(8, 8), facecolor=None)
 plt.imshow(wordcloud)
 plt.axis("off")
 plt.tight_layout(pad=0)
-plt.show()
 
-# Close the MongoDB connection
+plt.savefig(r'C:\UsC:\\Users\\donyo\\OneDrive\\Documents\\images\\methmi/WordCloud.png')
+plt.close()
+
 client.close()
 
 
