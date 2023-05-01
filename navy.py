5bb9186b7b0a805b6047539e24d038112fc5f579
yogi1754 <donyogeshwar@gmail.com>
2023-05-01 23:08:16 +0100

    Update navy.py

--- navy.py
+++ navy.py
@@ -4,6 +4,7 @@ import json
 import urllib.request
 import pandas as pd
 import pyodbc as pyodbc
+import visualize
 from pymongo import MongoClient
 import re
 import os
@@ -17,7 +18,8 @@ from sqlalchemy.dialects.mssql.information_schema import columns
 from textblob import TextBlob
 import os
 import matplotlib.pyplot as plt
-from wordcloud import WordCloud
+
+# from wordcloud import WordCloud
 
 nltk.download('stopwords')
 nltk.download('punkt')
@@ -26,11 +28,11 @@ nltk.download('punkt')
 collection_name = 'review_watches'
 uri = f"mongodb+srv://donyogeshwar:Welcome123@cluster0.xlzwbqj.mongodb.net/test"
 client = MongoClient(uri)
-database_name = 'amazon_reviews12'
+database_name = 'amazon_reviews'
 db = client[database_name]
 collection = db[collection_name]
 
-#db.review_watches.drop()
+# db.review_watches.drop()
 
 # Download and extract the dataset
 url = 'https://s3.amazonaws.com/amazon-reviews-pds/tsv/amazon_reviews_us_Watches_v1_00.tsv.gz'
@@ -40,7 +42,7 @@ with gzip.open(filename, 'rt', encoding='utf-8') as f:
     reader = csv.DictReader(f, delimiter='\t')
     for i, row in enumerate(reader):
         collection.insert_one(json.loads(json.dumps(row)))
-        if i == 1010:
+        if i == 101:
             break
 
 # Connect to mongodb and get data
@@ -60,7 +62,7 @@ df = pd.DataFrame(list(mydoc))
 
 
 # Clean the data
-def clean_data(df2):
+def clean_data(df):
     # Drop any rows with missing values
     df.dropna(inplace=True)
 
@@ -71,12 +73,12 @@ def clean_data(df2):
     df['review_date'] = pd.to_datetime(df['review_date'])
 
     # Remove any rows where the verified_purchase column is not 'Y' or 'N'
-    df1 = df[df['verified_purchase'].isin(['Y', 'N'])]
+    df = df[df['verified_purchase'].isin(['Y', 'N'])]
 
     # Remove any rows where the review_body or review_title columns are empty strings
-    df2 = df[df['review_body'] != '']
+    df = df[df['review_body'] != '']
 
-    return df2
+    return df
 
 
 def clean_text_df(df, columns, remove_stopwords=True):
@@ -97,7 +99,7 @@ def clean_text_df(df, columns, remove_stopwords=True):
     # Define function to clean a single text column
     stop_words = stopwords.words('english')
 
-    def clean_text(text, remove_stopwords=None):
+    def clean_text(text, remove_stopwords=True):
         # Convert to lowercase
         text = text.lower()
         # Remove URLs
@@ -117,9 +119,18 @@ def clean_text_df(df, columns, remove_stopwords=True):
 
     # Create new dataframe with cleaned text columns
     for col in columns:
-        df_clean[col] = df_clean[col].apply(clean_text)
+        df[col] = df[col].apply(clean_text)
 
-    return df_clean
+    return df
+
+
+df_clean = clean_data(df)
+
+# Clean 'review_body' and 'review_headline'
+text_columns = ['review_headline', 'review_body']
+df_clean = clean_text_df(df_clean, text_columns)
+
+df_clean.to_csv('cleaned_data.csv', index=False)
 
 # Define connection string
 server = '192.168.0.52,1433'
@@ -133,7 +144,7 @@ cnxn = pyodbc.connect(
 # Create table
 cursor = cnxn.cursor()
 
-#cursor.execute('DROP TABLE review_watches')
+# cursor.execute('DROP TABLE review_watches')
 
 cursor.execute("""
 CREATE TABLE review_watches (_id VARCHAR(255),
@@ -178,127 +189,75 @@ cnxn = pyodbc.connect(
 query = "SELECT * FROM review_watches"
 
 # Use pandas read_sql function to read data into dataframe
-visualize = pd.read_sql(query, cnxn)
+reviews_all = pd.read_sql(query, cnxn)
 
 # Close connection
 cnxn.close()
 
+# Define a dictionary mapping ratings to sentiments
+sentiments_dict = {5: "Positive", 4: "Positive", 3: "Neutral", 2: "Negative", 1: "Negative"}
 
-def sentiments(star_rating):
-    if star_rating in [1, 2]:
-        return 'Negative'
-    elif star_rating == 3:
-        return 'Neutral'
-    elif star_rating in [4, 5]:
-        return 'Positive'
+# Map the star_rating column to sentiments using the dictionary
+reviews_all["sentiment_category"] = reviews_all["star_rating"].map(sentiments_dict)
 
+save_dir = "C:\\Users\\donyo\\OneDrive\\Documents\\images\\navy\\"
 
-class WordCloud:
-    pass
+# Get the mean rating of the product
+mean_rating = reviews_all['star_rating'].mean()
 
+# Plot the histogram of the stars / ratings given by the customers
+plt.hist(reviews_all['star_rating'], bins=range(7))
+plt.xlabel('Star Rating')
+plt.ylabel('Count')
+plt.title('Histogram of Star Ratings')
+plt.savefig(os.path.join(save_dir, "hist_star_ratings.png"))
 
-def plot_reviews(reviews_all, save_plots, save_dir=None):
-    # Define save directory
-    if save_plots and save_dir is None:
-        save_dir = "C:\\Users\\donyo\\OneDrive\\Documents\\images\\navy/"
-        os.makedirs(save_dir, exist_ok=True)
-    elif save_plots and save_dir is not None:
-        os.makedirs(save_dir, exist_ok=True)
-    elif not save_plots:
-        save_dir = None
+# Create a bar chart for verified purchase and star_rating
+counts_verified_purchase = reviews_all.groupby(['star_rating', 'verified_purchase']).count()['review_id'].unstack()
+ax = counts_verified_purchase.plot.bar()
+ax.set_xlabel('Star rating')
+ax.set_ylabel('Count')
+ax.legend(title='Verified purchase', loc='upper left')
+plt.title('Count of Verified Purchases by Star Rating')
+plt.savefig(os.path.join(save_dir, 'bar_verified_purchases.png'))
 
-    # Create new column with value ['Positive', 'Neutral', 'Negative']
-    reviews_all["sentiment_category"] = reviews_all["star_rating"].apply(sentiments)
-    # Get the sentiment score for each review
-    reviews_all['sentiment'] = reviews_all['review_body'].apply(lambda x: TextBlob(x).sentiment.polarity)
-    # Get the mean rating of the product
-    mean_rating = reviews_all['star_rating'].mean()
+# replace missing values with an empty string
+# visualize['review_body'] = visualize['review_body'].fillna('')
 
-    # Plot the histogram of the stars / ratings given by the customers
+# apply the len() function using a lambda function
+# visualize['review_length'] = visualize['review_body'].apply(lambda x: len(x))
 
-    plt.hist(reviews_all['star_rating'], bins=range(7))
-    plt.xlabel('Star Rating')
-    plt.ylabel('Count')
-    plt.title('Histogram of Star Ratings')
-    if save_dir:
-        plt.savefig(os.path.join(save_dir, 'hist_star_ratings.png'))
-        plt.close()
+# group by star_rating and calculate the mean review length
+# df_grouped = visualize.groupby('star_rating')['review_length'].mean()
 
-        # Create a bar chart for verified purchase and star_rating
+# # Plot the bar chart
+# plt.figure(figsize=(10, 8))
+# plt.bar(df_grouped.index, df_grouped.values)
+# plt.title('Average Review Length by Star Rating')
+# plt.xlabel('Star Rating')
+# plt.ylabel('Average Review Length')
+# plt.savefig(os.path.join(save_dir, 'bar_review_length.png'))
 
-        counts_verified_purchase = reviews_all.groupby(['star_rating', 'verified_purchase']).count()[
-            'review_id'].unstack()
-        ax = counts_verified_purchase.plot.bar()
-        ax.set_xlabel('Star rating')
-        ax.set_ylabel('Count')
-        ax.legend(title='Verified purchase', loc='upper left')
-        plt.title('Count of Verified Purchases by Star Rating')
-        save_dir = 'path/to/save/dir'  # define save_dir here
-        if save_dir:
-            plt.savefig(os.path.join(save_dir, 'bar_verified_purchases.png'))
-            plt.close()
+cloud = WordCloud(background_color='gray', max_font_size=60,
+                  relative_scaling=1).generate(' '.join(reviews_all.review_body))
+plt.figure(figsize=(10, 8))
+plt.imshow(cloud, interpolation='bilinear')
+plt.axis('off')
+plt.title(f'Wordcloud for all Star Ratings')
+plt.savefig(os.path.join(save_dir, 'wordcloud.png'))
 
-        # Plot frequent words for all review
-        cloud = WordCloud(background_color='gray', max_font_size=60, relative_scaling=1).generate(
-            ' '.join(reviews_all.review_body))
-        plt.figure(figsize=(10, 8))
-        plt.imshow(cloud, interpolation='bilinear')
-        plt.axis('off')
-        plt.title(f'Wordcloud for all Star Ratings')
-        if save_dir:
-            plt.savefig(os.path.join(save_dir, 'wordcloud.png'))
-            plt.close()
+# Plot sentiment category
+cnt_sentiment = visualize['sentiment_category'].value_counts()
 
-        # Plot sentiment category
-        cnt_sentiment = reviews_all['sentiment_category'].value_counts()
+# Plot a pie chart of the sentiment categories
+plt.figure(figsize=(10, 8))
+labels = ['Positive', 'Neutral', 'Negative']
+sizes = [cnt_sentiment['Positive'], cnt_sentiment['Neutral'], cnt_sentiment['Negative']]
+colors = ['yellowgreen', 'gold', 'lightskyblue']
+plt.pie(sizes, labels=labels, colors=colors,
+        autopct='%1.1f%%', startangle=140)
+plt.axis('equal')
+plt.title('Distribution of Sentiment Categories')
+plt.savefig(os.path.join(save_dir, 'sentiment_pie.png'))
 
-        # Group the data by star_rating column
-        df_grouped = reviews_all.groupby('star_rating').count()
-
-        # Plot a bar chart of the groupby result
-        plt.bar(df_grouped.index, df_grouped['review_id'])
-        plt.xlabel('Star rating')
-        plt.ylabel('Count')
-        plt.title('Count of sentiments by Star Rating')
-        if save_dir:
-            plt.savefig(os.path.join(save_dir, 'bar_sentiments.png'))
-            plt.close()
-
-        # Plot the sentiment score distribution
-        plt.hist(reviews_all['sentiment'], bins=20)
-        plt.xlabel('Sentiment Score')
-        plt.ylabel('Count')
-        plt.title('Histogram of Sentiment Scores')
-        if save_dir:
-            plt.savefig(os.path.join(save_dir, 'hist_sentiment_scores.png'))
-            plt.close()
-
-        # Plot the mean sentiment score for each star rating
-        mean_sentiment_by_rating = reviews_all.groupby('star_rating')['sentiment'].mean()
-        plt.plot(mean_sentiment_by_rating.index, mean_sentiment_by_rating, '-o')
-        plt.xlabel('Star rating')
-        plt.ylabel('Mean Sentiment Score')
-        plt.title('Mean Sentiment Score by Star Rating')
-        if save_dir:
-            plt.savefig(os.path.join(save_dir, 'line_mean_sentiment_by_rating.png'))
-            plt.close()
-
-        # Plot the mean rating for each sentiment category
-        mean_rating_by_sentiment = reviews_all.groupby('sentiment_category')['star_rating'].mean()
-        plt.bar(mean_rating_by_sentiment.index, mean_rating_by_sentiment)
-        plt.xlabel('Sentiment Category')
-        plt.ylabel('Mean Rating')
-        plt.title('Mean Rating by Sentiment Category')
-        if save_dir:
-            plt.savefig(os.path.join(save_dir, 'bar_mean_rating_by_sentiment.png'))
-            plt.close()
-
-        # Print summary statistics
-        mean_rating = reviews_all['star_rating'].mean()
-        print(f"Number of reviews: {len(reviews_all)}")
-        print(f"Mean rating: {mean_rating:.2f}")
-        print(f"Number of positive reviews: {cnt_sentiment['Positive']}")
-        print(f"Number of neutral reviews: {cnt_sentiment['Neutral']}")
-        print(f"Number of negative reviews: {cnt_sentiment['Negative']}")
-
-        client.close()
+client.close()
